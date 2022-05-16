953287b050f306c3e039aae76cd73172f8188885
janio <2207597040@qq.com>
2022-05-16 16:08:12 +0800

    utility 0.1

--- utility.js
+++ utility.js
@@ -1,48 +1,53 @@
-//防抖
-function debounce(fn, wait) {
-    let timer = null;
-    return function() {
-        if (timer) {
-            clearTimeout(timer);
-        }
-        timer = setTimeout(() => {
-            fn.apply(this, arguments);
-        }, wait);
-    }
-}
-
-
-
-//节流
-function throttle(fn, wait) {
-    let timer = null;
-    return function() {
-        timer = setTimeout(() => {
+let utility = (function() {
+    //防抖
+    function debounce(fn, wait) {
+        let timer = null;
+        return function() {
             if (timer) {
-                return;
+                clearTimeout(timer);
             }
-            fn.apply(this, arguments);
-            timer = null;
-        }, wait);
+            timer = setTimeout(() => {
+                fn.apply(this, arguments);
+            }, wait);
+        }
     }
-}
-//offsetLeft
-function offsetLeft(currElement) {
-    let left = currElement.offsetLeft;
-    while (currElement.offsetParent !== null) {
-        currElement = currElement.offsetParent;
-        left += currElement.offsetLeft;
-        left += parseFloat(getComputedStyle(currElement, null).borderLeft);
+    //节流
+    function throttle(fn, wait) {
+        let timer = null;
+        return function() {
+            timer = setTimeout(() => {
+                if (timer) {
+                    return;
+                }
+                fn.apply(this, arguments);
+                timer = null;
+            }, wait);
+        }
     }
-    return left;
-}
-//offsetTop
-function offsetTop(currElement) {
-    let top = currElement.offsetTop;
-    while (currElement.offsetParent !== null) {
-        currElement = currElement.offsetParent;
-        top += currElement.offsetTop;
-        top += parseFloat(getComputedStyle(currElement, null).borderTop);
+    //offsetLeft
+    function offsetLeft(currElement) {
+        let left = currElement.offsetLeft;
+        while (currElement.offsetParent !== null) {
+            currElement = currElement.offsetParent;
+            left += currElement.offsetLeft;
+            left += parseFloat(getComputedStyle(currElement, null).borderLeft);
+        }
+        return left;
     }
-    return top;
-}
+    //offsetTop
+    function offsetTop(currElement) {
+        let top = currElement.offsetTop;
+        while (currElement.offsetParent !== null) {
+            currElement = currElement.offsetParent;
+            top += currElement.offsetTop;
+            top += parseFloat(getComputedStyle(currElement, null).borderTop);
+        }
+        return top;
+    }
+    return {
+        debounce,
+        throttle,
+        offsetLeft,
+        offsetTop
+    }
+})();
